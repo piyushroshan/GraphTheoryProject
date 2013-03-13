36ae715f46089220a56ebfba57e91c79f4f14072
Roshan Piyush <piyushroshan@gmail.com>
2013-03-13 16:26:09 +0530

    file i/o fixed

--- src/gtc/assignment/piyush/FileOutput.java
+++ src/gtc/assignment/piyush/FileOutput.java
@@ -4,34 +4,38 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 public class FileOutput {
-	String filename ;
+	String filename;
 	PrintWriter writer = null;
-	public FileOutput(String filename){
+
+	public FileOutput(String filename) {
 		this.filename = filename;
 		try {
 			writer = new PrintWriter(filename);
+			System.out.println("created\t" + filename + "\t sucessfully");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			System.out.println("notfound");
+			System.out.println("could not create\t" + filename);
+			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
 	}
-	public void writeTextFile (String s) {
+
+	public void writeTextFile(String s) {
 		writer.println(s);
 	}
 
 	public void writeTextFile(int[][] a, int n) {
-			writer.print(" | ");
-			for (int i = 0; i < n; i++)
-				writer.print(i + 1 + " ");
-			writer.println();
-			for (int i = 0; i < n; i++) {
-				writer.print(i + 1 + "| ");
-				for (int j = 0; j < n; j++) {
-					writer.print(a[i][j] + " ");
-				}
-				writer.println("");
+		writer.print(" | ");
+		for (int i = 0; i < n; i++)
+			writer.print(i + 1 + " ");
+		writer.println();
+		for (int i = 0; i < n; i++) {
+			writer.print(i + 1 + "| ");
+			for (int j = 0; j < n; j++) {
+				writer.print(a[i][j] + " ");
 			}
+			writer.println("");
+		}
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
